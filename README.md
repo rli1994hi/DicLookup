13b67a6ec28e6fe456c06af9d6ca385d30d31e5f
rli1994hi <rli1994hi@gmail.com>
2021-04-01 22:41:32 -0700

    [Bugfix] Handle words with multiple meanings
    Fixes #4
    
    This change handles word with multiple meanings, e.g.: present.

--- README.md
+++ README.md
@@ -3,6 +3,35 @@ Look up explanation for a list of words and output in CSV.
 
 Dictionary API used in this script: https://dictionaryapi.dev/. Thanks.
 
+## Usage: 
+(Standing where word_lookup.rb is at)
+
+```
+$ ruby word_lookup.rb -w hello,hi,asdf
+```
+
+If a word has multiple distinct meanings, e.g.: present, the tools seperates them with "----------". Detail see the sample CSV in [Output section](#Output)
+
+
+## Output
+
+The output is a csv file named as `output.csv`. It's stored under the same directory as `word_lookup.rb`.
+
+Terminal Output Sample:
+
+```
+$ ruby word_lookup.rb -w candy,sadf,hi,present
+done with candy
+No definition found for: [sadf]. Check the spelling, or google the word directly. Skipping.
+done with hi
+done with present
+========================================
+The definitions of valid words in the input word list is stored in output.csv.
+```
+
+Sample CSV: [google spread sheet](https://docs.google.com/spreadsheets/d/1A4Hy_qq1nmuQZKlCCmQF0pUMPuVJCCcaBFAlmYYps0A/edit?usp=sharing)
+
+
 ## Env Requirements
 Test env:
 1. ruby: ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-darwin16]
@@ -10,27 +39,3 @@ Test env:
 
 Extra:
 Gem [httparty](https://github.com/jnunemaker/httparty) has been installed.
-
-
-## Usage: 
-(Standing where dic_lookup.rb is at) 
-
-```
-$ ruby dic_lookup.rb -w hello,hi,asdf
-```
-
-## Output
-
-The output is a csv file named as `output.csv`. It's stored under the same directory as `dic_lookup.rb`.
-
-Output Sample:
-
-```
-$ ruby dic_lookup.rb -w hello,hi,sadf
-done with hello
-done with hi
-wrong spelling: sadf
-========================================
-The definitions of valid words in the input word list is stored in output.csv.
-```
-
--- word_lookup.rb
+++ word_lookup.rb
@@ -1,4 +1,4 @@
-# Usage: (Standing where dic_lookup.rb is at) ruby dic_lookup.rb -w hello,hi,dsdf
+# Usage: (Standing where word_lookup.rb is at) ruby word_lookup.rb -w hello,hi,dsdf
 
 # TODO: 
 # 1. Seems PART_OF_SPEECH_MAP is output as files
@@ -15,20 +15,40 @@ PART_OF_SPEECH_MAP = {"adjective" => "adj.", "adverb" => "adv.", "noun" => "n.",
 DEF_NOT_FOUND_STRINGS = ["Word not found", "No Definitions Found"]
 
 def grab_def(word_list)
-  word_def_list = []
+  top_word_def_list = [] 
 
   word_list.each do |word|
-    word_def = SingleWordDef.new
-    word_def.word = word
-
     response = get_response(construct_url(word))
     unless has_def?(response)
       puts "No definition found for: [#{word}]. Check the spelling, or google the word directly. Skipping.\n"
       next
     end
 
-    response = JSON.parse(response).first
+    top_word_def = TopWordDef.new
+    top_word_def.word = word
 
+    # word_def = SingleWordDef.new
+    # word_def.word = word    
+
+    # response = JSON.parse(response).first
+    responses = JSON.parse(response)
+
+    responses.each do |response|
+        parse_single_response(response, top_word_def)
+    end
+
+    top_word_def_list << top_word_def
+    puts "done with #{word}"
+  end
+
+  # top_word_def_list.each do |top_word_def|
+  #   puts top_word_def.def_list_str
+  # end
+  output_word_def_list(top_word_def_list)
+end
+
+def parse_single_response(response, top_word_def)
+    word_def = SingleWordDef.new(top_word_def.word)
     phonetic = response["phonetic"]
     word_def.phonetic = phonetic
     meaning_list = response["meanings"]
@@ -45,10 +65,7 @@ def grab_def(word_list)
     end
 
     word_def.def_str = def_str.strip!
-    word_def_list << word_def
-    puts "done with #{word}"
-  end
-  output_word_def_list(word_def_list)
+    top_word_def.def_list << word_def
 end
 
 def has_def?(response)
@@ -89,10 +106,10 @@ def def_list_str(def_list)
 end
 
 
-def output_word_def_list(word_def_list)
+def output_word_def_list(top_word_def_list)
   c = CSV.generate do |csv|
-    word_def_list.each do |word_def|
-      csv << [word_def.word, word_def.def_str, word_def.phonetic]
+    top_word_def_list.each do |top_word_def|
+      csv << [top_word_def.word, top_word_def.def_list_str]
     end
   end
 
@@ -100,6 +117,22 @@ def output_word_def_list(word_def_list)
 
   puts "========================================"
   puts "The definitions of valid words in the input word list is stored in output.csv."
+end
+
+class TopWordDef
+    # word: the word that the tool is working for
+    # def_list: a list of SingleWordDef
+    attr_accessor :word, :def_list
+
+    def initialize(word = nil, def_list = nil)
+        @word = word
+        @def_list = def_list || []
+    end
+
+    def def_list_str
+        def_str_list = @def_list.map { |word_def| word_def.def_str }
+        def_str_list.join("\n\n----------------\n\n")
+    end
 end
 
 class SingleWordDef
